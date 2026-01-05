7fc591474e827d6fc638e64026a5c95c85f17e56
Смородинов Павел <look1899@mail.ru>
2026-01-06 00:41:57 +0500

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,13 @@ import './index.css';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from 'store/store';
+import { ThemeProvider } from 'skb_uikit';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Provider>,
 );
